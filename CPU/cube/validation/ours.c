82207be5706ac32b8ef3e911d32102a44da534f8
moehabib <moe.habib9@gmail.com>
2013-03-10 07:21:46 +0000

    added unit tests for the cube

--- CPU/cube/validation/ours.c
+++ CPU/cube/validation/ours.c
@@ -3,28 +3,31 @@
 #include "math.h"
 
 //rubik's cube libs
-#include "parse.c"
-#include "move.c"
+#include "../parse.c"
+#include "../move.c"
 
 
-int main() {
-
+int main(int argc,char ** argv) {
+	
+   
    INIT_CUBE_MOVES();
    INIT_CUBE_PARSE();
 
+int i;
+
    CubePos base;
    cube_init(&base);
-   print_corners_and_edges(&base);
-   //move(&base, RIGHT);
-   move(&base, RIGHT);
-   print_corners_and_edges(&base);
+   //print_corners_and_edges(&base);
+   
+
+  for (i=1; i< argc; i++) {
+    move(&base, (int)atoi(argv[i]));
+}
+  // print_corners_and_edges(&base);
 
    char *sm = "UF UR UB UL DF DR DB DL FR FL BR BL UFR URB UBL ULF DRF DFL DLB DBR ";
    const char *r;
-   r = "asdas";
-
-
-
+   
    //r = parse_Singmaster(&base, sm);
 
    char *a = Singmaster_string(&base);
---
+++ CPU/cube/validation/sm_test.c
@@ -0,0 +1,42 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+
+//rubik's cube libs
+#include "../parse.c"
+#include "../move.c"
+
+
+int main(int argc,char ** argv) {
+	
+   
+   INIT_CUBE_MOVES();
+   INIT_CUBE_PARSE();
+
+   int i;
+
+
+   CubePos base;
+   CubePos test;
+   cube_init(&base);
+   //print_corners_and_edges(&base);
+  
+  
+  for (i=1; i< argc; i++) {
+	  move(&base, (int)atoi(argv[i]));
+  }
+   
+   parse_Singmaster(&test, Singmaster_string(&base));
+   int n = memcmp(&test,&base, 20);
+
+   if ( n == 0 ) {
+   	printf("1");
+
+   }
+   else {
+	printf("0");
+
+   }
+
+
+}
