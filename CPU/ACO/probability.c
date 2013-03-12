4cd8cbd45eea78c414f84d142a35f32722bd3bd6
Haithem <haithem.fendi@devinci.fr>
2013-03-12 11:29:43 +0000

    fix some mistakes and made the constant names more clear

--- CPU/ACO/probability.c
+++ CPU/ACO/probability.c
@@ -1,6 +1,3 @@
-
-
-
 void update_prob(int * G, float * T, float * P, int size, float * sum)
 {
     int i,j,index;
@@ -9,9 +6,9 @@ void update_prob(int * G, float * T, float * P, int size, float * sum)
         for(j=0 ; j<size ; j++)
         {
             index = size*i + j;
-            if(i < j)
+            if(G[index] != 0 && T[index] != 0)
             {
-                P[index] = pow(T[index],alpha) * pow(1/G[index],beta)/sum[i];
+                P[index] = pow(T[index],ALPHA) * pow(1/G[index],BETA)/sum[i];
             }
             else{
                 P[index] = 0;
@@ -30,12 +27,11 @@ float * sum_prob(int * G, float * T, int size)
         for(j=0 ; j<size ; j++)
         {
             index = size*i + j;
-            if(i < j){
-                sum[i] += pow(T[index],alpha) * pow(1/G[index],beta);
+            if(G[index] != 0 && T[index] != 0){
+                sum[i] += pow(T[index],ALPHA) * pow(1/G[index],BETA);
             }
         }
     }
     return sum;
 }
-
 
